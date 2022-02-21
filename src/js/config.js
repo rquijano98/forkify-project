ea8f87265b701495445d3b8678e68eb323f9c0ae
rq2686839 <riley@quijano.net>
2022-02-21 09:13:04 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = '48509875-106c-4f74-aaf1-3cb294616c0a';
+
+export const MODAL_CLOSE_SEC = 2.5;
